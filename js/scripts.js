ecadf2ba6c91c36cbe880f4bce90ef3bec188c96
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-21 16:35:39 -0700

    added everything based off of address book

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function PlaceLog(){
 }
 
 PlaceLog.prototype.addPlace = function(place){
-  place.id  = this.assignId();
+  place.id = this.assignID();
   this.places[place.id] = place;
 };
 
@@ -21,7 +21,7 @@ PlaceLog.prototype.findPlace = function(id){
   return false;
 };
 
-PlaceLog.prototype.deleteContact = function(id){
+PlaceLog.prototype.deletePlace = function(id){
   if(this.places === undefined) {
     return false;
   }
@@ -42,3 +42,50 @@ function Places(name, landmark, location, notes){
 
 //ui logic
 let placeLog = new PlaceLog();
+
+function displayPlaceDetails(placeLogToDisplay) {
+  let placeList = $("ul#placeNames");
+  let htmlForPlaceInfo = "";
+  Object.keys(placeLogToDisplay.places).forEach(function(key) {
+    const place = placeLogToDisplay.findPlace(key);
+    htmlForPlaceInfo += "<li id=" + place.id + ">" + place.name + "</li>";
+  });
+  placeList.html(htmlForPlaceInfo);
+}
+
+function showPlace(placeID){
+  const place = placeLog.findPlace(placeID);
+  $(".show-places").show();
+  $(".place-name").html(place.name);
+  $(".landmark").html(place.landmark);
+  $(".location").html(place.location);
+  $(".notes").html(place.notes);
+  let buttons = $("#buttons");
+  buttons.append("<button class='deleteButton' id=" + place.id + ">Delete</button>");
+}
+
+function attachPlaceListeners() {
+  $("ul#placeNames").on("click", "li", function() {
+    showPlace(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    placeLog.deletePlace(this.id);
+    $(".show-places").hide();
+    displayPlaceDetails(placeLog);
+  });
+}
+
+$(document).ready(function() {
+  attachPlaceListeners();
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const inputtedName = $("#new-name").val();
+    const inputtedLandmark = $("#new-landmark").val();
+    const inputtedLocation = $("#new-location").val();
+    const inputtedNotes = $("#new-notes").val();
+
+    let newPlace = new Places(inputtedName, inputtedLandmark, inputtedLocation, inputtedNotes);
+    placeLog.addPlace(newPlace);
+    displayPlaceDetails(placeLog);
+  });
+});
